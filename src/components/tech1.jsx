60b6bc4c099c8f2c124f765a74a8eadbeb4172c2
Abdul Wahab <abdulwahabee5@gmail.com>
2024-09-22 18:43:59 +0500

    skill icons small

--- src/components/tech1.jsx
+++ src/components/tech1.jsx
@@ -7,7 +7,7 @@ const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology)=>(
-        <div className='w-20 h-20' key={technology.name}>
+        <div className='w-16 h-16' key={technology.name}>
           <img src={technology.icon}/>
         </div>
       ))}
